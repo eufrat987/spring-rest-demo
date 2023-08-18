299db57e487d4d2ea25e5bde4268fb5b964e5b1f
luka <bizeufrat@gmail.com>
2023-08-18 21:41:23 +0200

    Add binding example

---
+++ src/main/java/org/example/restcontroller/BindExample.java
@@ -0,0 +1,18 @@
+package org.example.restcontroller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bind")
+public class BindExample {
+
+    @GetMapping
+    public String printName(@RequestParam(required = false) String name) {
+        return name != null ? "Hello " + name : "Please provide name please :D";
+    }
+
+    @GetMapping("/default")
+    public String printNameOrDefault(@RequestParam(defaultValue = "set name please :D") String name) {
+        return "Hello " + name;
+    }
+}
